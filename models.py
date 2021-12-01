61fa96586dfa2a64f9302e3b814c0e7f1360dd17
KidusAlemayehu <kidusalemayehu705@gmail.com>
2021-12-01 19:21:50 +0300

    On Branch Main

--- models.py
+++ models.py
@@ -2,8 +2,19 @@ from app import db
 
 class Video(db.Model):
     __tablename__ = 'video'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     genre = db.Column(db.String(50))
     description = db.Column(db.String(150))
     released_year = db.Column(db.Datetime())
+    num_of_amounts = db.Column(db.Integer())
+    is_available = db.Column(db.Boolean())
+
+    def __repr__(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "genre": self.genre,
+            "description": self.description,
+            "released_year": self.released_year
+        }
--- resources/video.py
+++ resources/video.py
@@ -1,1 +1,4 @@
-pass
+from flask import request
+from http import HTTPStatus
+from models import Video
+from flask_restful import Resource
