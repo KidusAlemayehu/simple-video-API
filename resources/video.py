f1a6cf59b8a70d4ab75dce3a79373984cd76a466
KidusAlemayehu <kidusalemayehu705@gmail.com>
2021-12-01 19:28:33 +0300

    On Branch Main

--- resources/video.py
+++ resources/video.py
@@ -2,3 +2,12 @@ from flask import request
 from http import HTTPStatus
 from models import Video
 from flask_restful import Resource
+
+class VideoListResource(Resource):
+    pass
+
+class VideoResource(Resource):
+    pass
+
+class VideoAvailability(Resource):
+    pass
